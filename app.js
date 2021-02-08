70a16ebae6ce6ce0c23707b7ca7308176decb2e7
Sean McMahon <sean.mcmahon87@gmail.com>
2021-02-08 19:03:54 +0000

    load more functionality added

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 let searchValue;
+const more = document.querySelector('.more');
+let page = 1;
+let fetchLink = "";
+let currentSearch;
 
 //EVENT LISTENERS
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
-})
+});
+more.addEventListener('click', loadMore);
 
 function updateInput(e) {
   searchValue = e.target.value;
@@ -34,18 +40,48 @@ function generatePictures(data) {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
     galleryImg.innerHTML = `<img src= ${photo.src.large}></img>
-    <p>${photo.photographer}</p>`;
+    <div class='gallery-info'>
+    <p>${photo.photographer}</p>
+    <a href=${photo.src.original}>Full Size</a>
+    </div>
+    `;
     gallery.appendChild(galleryImg);
   })
 } 
 
 async function curatedPhotos() {
-  const data = await fetchApi('https://api.pexels.com/v1/curated')
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(query){
-  const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`)
+  //empty gallery
+  clear();
+  //repopulate gallery using search term
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+//empties gallery
+function clear() {
+  gallery.innerHTML = '';
+  searchInput.value = '';
+}
+
+async function loadMore() {
+  //increase page number
+  page++;
+  //check to see if there is a current search
+  if (currentSearch) {
+    //if so, load more search results
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    //if not, load more featured images
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
